48eba657418f725b9b2248d8b76e77d9a7cce747
modusami <128193218+modusami@users.noreply.github.com>
2024-08-04 23:02:54 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
+import TodoPage from "@/components/pages/TodoPage";
 import Image from "next/image";
 
 export default function Home() {
-	return <div>Hello</div>;
+	return (
+		<main className="min-h-screen p-24 flex flex-col items-center">
+			{/* nav bar / title */}
+			<nav>
+				<h1>Todo List @React | @Node | @Postgres</h1>
+			</nav>
+			{/* todos */}
+			<div className="flex-1">
+				<TodoPage />
+			</div>
+
+			<footer>
+				<b>Michael-Andre Odusami</b>
+			</footer>
+		</main>
+	);
 }
