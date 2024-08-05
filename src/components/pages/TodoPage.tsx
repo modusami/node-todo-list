7336a9e15760d5ed3aa6b4fceeb3f7642e566b61
modusami <128193218+modusami@users.noreply.github.com>
2024-08-04 23:24:02 -0400

    Update TodoPage.tsx

--- src/components/pages/TodoPage.tsx
+++ src/components/pages/TodoPage.tsx
@@ -1,5 +1,19 @@
+import Todo from "../ui/Todo";
+
 const TodoPage = () => {
-	return <section></section>;
+	return (
+		<section className="mt-10 mb-5">
+			<div>
+				<Todo
+					id={0}
+					title={"First Todo"}
+					notes={"This is my very own first todo"}
+					isCompleted={true}
+					isFavorite={true}
+				/>
+			</div>
+		</section>
+	);
 };
 
 export default TodoPage;
