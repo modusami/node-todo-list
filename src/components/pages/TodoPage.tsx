84e335ddbf172efb44266cd9fdf0e8c3c523f8c9
modusami <128193218+modusami@users.noreply.github.com>
2024-08-04 23:36:22 -0400

    Update TodoPage.tsx

--- src/components/pages/TodoPage.tsx
+++ src/components/pages/TodoPage.tsx
@@ -3,11 +3,71 @@ import Todo from "../ui/Todo";
 const TodoPage = () => {
 	return (
 		<section className="mt-10 mb-5">
-			<div>
+			<div className="space-y-3 h-[500px] overflow-y-scroll custom-scrollbar">
 				<Todo
 					id={0}
 					title={"First Todo"}
 					notes={"This is my very own first todo"}
+					isCompleted={true}
+					isFavorite={true}
+				/>
+				<Todo
+					id={1}
+					title={"Register For Virginia Tech Courses"}
+					notes={
+						"Need to take CS 2104 ASAP, but look for another tech based course I can potentially take at tech that is actually meaningful. No BS classes such as sociology and such."
+					}
+					isCompleted={true}
+					isFavorite={true}
+				/>
+
+				<Todo
+					id={0}
+					title={"First Todo"}
+					notes={"This is my very own first todo"}
+					isCompleted={true}
+					isFavorite={true}
+				/>
+				<Todo
+					id={1}
+					title={"Register For Virginia Tech Courses"}
+					notes={
+						"Need to take CS 2104 ASAP, but look for another tech based course I can potentially take at tech that is actually meaningful. No BS classes such as sociology and such."
+					}
+					isCompleted={true}
+					isFavorite={true}
+				/>
+
+				<Todo
+					id={0}
+					title={"First Todo"}
+					notes={"This is my very own first todo"}
+					isCompleted={true}
+					isFavorite={true}
+				/>
+				<Todo
+					id={1}
+					title={"Register For Virginia Tech Courses"}
+					notes={
+						"Need to take CS 2104 ASAP, but look for another tech based course I can potentially take at tech that is actually meaningful. No BS classes such as sociology and such."
+					}
+					isCompleted={true}
+					isFavorite={true}
+				/>
+
+				<Todo
+					id={0}
+					title={"First Todo"}
+					notes={"This is my very own first todo"}
+					isCompleted={true}
+					isFavorite={true}
+				/>
+				<Todo
+					id={1}
+					title={"Register For Virginia Tech Courses"}
+					notes={
+						"Need to take CS 2104 ASAP, but look for another tech based course I can potentially take at tech that is actually meaningful. No BS classes such as sociology and such."
+					}
 					isCompleted={true}
 					isFavorite={true}
 				/>
